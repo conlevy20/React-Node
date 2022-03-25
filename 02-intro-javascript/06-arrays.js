b918c2b3a232c252b58ce1301e062c4804739b29
conlevy20 <dev.conlevy18@gmail.com>
2022-03-25 09:51:23 +0300

    Updated javascript arrays lesson file

--- 02-intro-javascript/06-arrays.js
+++ 02-intro-javascript/06-arrays.js
@@ -32,15 +32,15 @@ const mainArr = [
 ];
 
 const arrElement = mainArr[0];
-console.log(mainArr[0]);
-console.log(mainArr[0][0]);
+console.log(arrElement[0]);
+console.log(arrElement[0][0]);
 
-// console.log(mainArr[0][0]);
+console.log(mainArr[0][0]);
 
 /* Array Methods */
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-// push - Adds elements at the end of the array
+// push - Adds elements at the end of the array and returns the updated length of the array
 const arr1 = [];
 
 arr1.push('Hello');
@@ -48,7 +48,13 @@ arr1.push('World', 'You are great');
 
 console.log(arr1);
 
-// pop - Removes elements from the end of the array
+const push = (arr, value) => {
+    arr[arr.length] = value;
+
+    return arr.length;
+};
+
+// pop - Removes an element from the end of the array and returns the removed element
 const arr2 = [10, 20, 30, 40, 50];
 
 arr2.pop();
@@ -56,6 +62,13 @@ console.log(arr2);
 
 const poppedEl = arr2.pop();
 console.log(poppedEl);
+
+const pop = (arr) => {
+    const poppedElement = arr[arr.length - 1];
+    arr[arr.length - 1] = undefined;
+
+    return poppedElement;
+};
 
 // unshift - Adds elements to the start of the array
 const arr3 = [20, 30];
@@ -65,7 +78,21 @@ arr3.unshift(0, 5);
 
 console.log(arr3);
 
-// shift - Removes elemenets from the beginning of the array
+const unshift = (arr, value) => {
+    for (let i = arr.length; i >= 0; i--) {
+        if (i === 0) {
+            arr[i] = value;
+            break;
+        }
+
+        arr[i] = arr[i - 1];
+        arr[i - 1] = undefined;
+    }
+
+    return arr.length;
+};
+
+// shift - Removes elemenets from the beginning of the array and returns the removed element
 const arr4 = [10, 20, 40, 50];
 
 arr4.shift();
@@ -74,11 +101,34 @@ console.log(shiftedEl);
 
 console.log(arr4);
 
+const shift = (arr) => {
+    const shiftedElement = arr[0];
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        arr[i] = arr[i + 1];
+        arr[i + 1] = undefined;
+    }
+
+    return shiftedElement;
+};
+
 // includes - Checks if an element exists in the array
 const arr5 = [1986, 1987, 1999];
 
 console.log(arr5.includes(1999)); // true
 console.log(arr5.includes(1989)); // false
+
+const includes = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+
+        if (element === value) {
+            return true;
+        }
+    }
+
+    return false;
+};
 
 // indexOf - Returns the index of an element, if the element does not exist it will return -1
 const arr6 = ['dog', 'cat', 'rabbit', 'sheep', 'dog', 'pig'];
@@ -87,13 +137,37 @@ console.log(arr6.indexOf('dog')); // 0
 console.log(arr6.indexOf('dog', 3)); // 4
 console.log(arr6.indexOf('giraffe')); // -1
 
+const indexOf = (arr, searchElement, fromIndex) => {
+    if (fromIndex && fromIndex > arr.length - 1) {
+        return -1;
+    }
+
+    for (let i = fromIndex ? fromIndex : 0; i < arr.length; i++) {
+        const element = arr[i];
+
+        if (element === searchElement) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
 // at - Returns the element at the index you specified
 const arr7 = ['dog', 'cat', 'rabbit', 'sheep', 'pig'];
 
-// console.log(arr7.at(2)); // rabbit
-// console.log(arr7.at(arr7.length - 1)); // pig
-// console.log(arr7.at(-1)); // pig
-// console.log(arr7.length(100)); // undefined
+console.log(arr7.at(2)); // rabbit
+console.log(arr7.at(arr7.length - 1)); // pig
+console.log(arr7.at(-1)); // pig
+console.log(arr7.length(100)); // undefined
+
+const at = (arr, index) => {
+    if (index < 0) {
+        return arr[arr.length + index];
+    }
+
+    return arr[index];
+};
 
 // concat - Combines multiple arrays into 1
 const array1 = ['a', 'b', 'c', 'd'];
@@ -105,11 +179,34 @@ console.log(array1.concat(array2));
 const newArray = array1.concat(array2, array3);
 console.log(newArray);
 
+const concat = (arr1, arr2) => {
+    const combinedArr = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        combinedArr.push(arr1[i]);
+    }
+
+    for (let i = 0; i < arr2.length; i++) {
+        combinedArr.push(arr2[i]);
+    }
+
+    return combinedArr;
+};
+
 // reverse - Reverse the elements in the array
 const arr8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 arr8.reverse();
 console.log(arr8);
+
+const reverse = (arr) => {
+    const reversedArr = [];
+
+    for (let i = arr.length - 1; i >= 0; i++) {
+        reversedArr.push(arr[i]);
+    }
+    return reversedArr;
+};
 
 // sort - Sorts the elements in the array
 const months = ['March', 'Jan', 'Feb', 'Dec'];
@@ -165,6 +262,18 @@ newArr12.map((element, index) => {
     console.log(element, index);
 });
 
+const map = (arr, callback) => {
+    const updatedArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+
+        const updatedElement = callback(element, i);
+        updatedArray.push(updatedElement);
+    }
+    return updatedArray;
+};
+
 // find - finds the first element based on a condition
 const persons = [
     { name: 'Al', age: 22 },
@@ -173,18 +282,20 @@ const persons = [
     { name: 'Yair', age: 50 },
 ];
 
-const found = persons.find((element) => element.name === 'Yair');
+const found = persons.find((element, index) => element.name === 'Yair');
 console.log(found);
 
-// const findElement = (arr) => {
-//     for (let i = 0; i < persons.length; i++) {
-//         const element = persons[i];
+const find = (arr, callback) => {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
 
-//         if (element.name === 'Yair') {
-//             return element;
-//         }
-//     }
-// };
+        const isElementFound = callback(element, i);
+
+        if (isElementFound) {
+            return element;
+        }
+    }
+};
 
 // filter - finds all the elements based on a condition
 const persons2 = [
@@ -197,16 +308,18 @@ const persons2 = [
 const yairs = persons2.filter((element) => element.firstName === 'Yair' || element.lastName === 'Yair');
 console.log(yairs);
 
-// const filterArr = (arr) => {
-//     const filteredArray = [];
+const filter = (arr, callback) => {
+    const filteredArray = [];
 
-//     for (let i = 0; i < arr.length; i++) {
-//         const element = arr[i];
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
 
-//         if (element.firstName === 'Yair' || element.lastName === 'Yair') {
-//             filteredArray.push(element);
-//         }
-//     }
+        const isFound = callback(element);
 
-//     return filteredArray;
-// };
+        if (isFound) {
+            filteredArray.push(element);
+        }
+    }
+
+    return filteredArray;
+};
