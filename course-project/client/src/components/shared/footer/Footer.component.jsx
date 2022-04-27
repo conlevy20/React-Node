8edb69ff297c6d0fff11f50a1f55fe57fc2c5718
conlevy20 <dev.conlevy18@gmail.com>
2022-04-27 15:35:37 +0300

    Added footer component to course project

--- course-project/client/src/components/shared/footer/Footer.component.jsx
+++ course-project/client/src/components/shared/footer/Footer.component.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './footer.styles.css';
 
+// The footer component needs to take 100% of the window and its height should be 60px;
+// The footer needs to have a span inside the element: Developed by "Your name".
+// The span needs to be in the middle.
+// Add a background color, add a text color.
+
 const Footer = () => {
     return null;
 };
