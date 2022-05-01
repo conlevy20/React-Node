b39b0092fd222aa88fadb94414dcc3ff97250f3f
conlevy20 <dev.conlevy18@gmail.com>
2022-05-01 20:18:35 +0300

    Added page not found assignment

---
+++ course-project/client/src/pages/page-not-found/PageNotFound.component.jsx
@@ -0,0 +1,10 @@
+/* 
+    Create a page not found/404 page. 
+
+    The 404 page should take 100% of the window width and it should have a 
+    minimum height of 100% of the window height - 80px. 
+
+    In the middle of the 404 page should contain the following - 
+    1. A title: 404
+    2. A button with a modern design that says - Go Back Home
+*/
